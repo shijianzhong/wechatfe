218863f5a4e5d9bf8f857fe1059b8680d9a5d6ee
jianzhong.shi <jianzhong.shi@vcg.com>
2018-05-24 17:35:37 +0800

    修改登陆方式

--- src/view/login.vue
+++ src/view/login.vue
@@ -1,21 +1,32 @@
 <template>
-    <div class="login_mainwrap">
-
+    <div class="login_mainwrap" id="loginmainwrap">
+        <img :src="imgurl" alt="">
     </div>
 </template>
 <script>
-import {getLoginUrl} from '../../api'
+import { getLoginUrl } from "../../api";
+import QRCode from "qrcode";
 export default {
-    data(){
-        return{
-
-        }
-    },
-    created(){
-        getLoginUrl().then(x=>{
-            console.log(x);
-        })
-    }
-}
+  data() {
+    return {
+      imgurl:"",
+      loginurl
+    };
+  },
+  created() {
+    getLoginUrl().then(x => {
+      console.log(x);
+    });
+  },
+  mounted() {
+    QRCode.toDataURL("I am a pony!")
+      .then(url => {
+        this.imgurl = url;
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+};
 </script>
 
